03a1b808d81c78e5b0c89189b13ce1fad5173a22
ladislao <ladislaox@gmail.com>
2019-06-02 20:47:49 -0500

    maquetado boton de formulario

--- public/views/home/form.php
+++ public/views/home/form.php
@@ -1,28 +1,31 @@
 <div class="sf-form">
     <form action="">
-        <h4>SOLICITA INFORMACIÓN Y OBTÉN UN <strong>5% DE DESCUENTO</strong></h4>
-        <p>( Incluye plano de distribución )</p>
-        <div class="input">
-            <input type="text" placeholder="Nombre">
-        </div> 
-        <div class="input">
-            <input type="text" placeholder="Correo"> 
-        </div>
-        <div class="input">
-            <input type="text" placeholder="Telefono">
-        </div>
-        <div class="select">
-            <select>
-                <option value="">- Selecione Area-</option>
-            </select>
-        </div>
+        <div class="form-top">
+            <h4>SOLICITA INFORMACIÓN Y OBTÉN UN <strong>5% DE DESCUENTO</strong></h4>
+            <p>( Incluye plano de distribución )</p>
+            <div class="input">
+                <input type="text" placeholder="Nombre">
+            </div> 
+            <div class="input">
+                <input type="text" placeholder="Correo"> 
+            </div>
+            <div class="input">
+                <input type="text" placeholder="Telefono">
+            </div>
+            <div class="select">
+                <select>
+                    <option value="">- Selecione Area-</option>
+                </select>
+            </div>
 
-        <div class="textarea">
-            <textarea placeholder="Hola estoy interesado (a), solicito más información." cols="30" rows="5"></textarea>
+            <div class="textarea">
+                <textarea placeholder="Hola estoy interesado (a), solicito más información." cols="30" rows="5"></textarea>
+            </div>
         </div>
-
-        <div class="sf_btn">
-            <button class="btn-content-solid-2" type="submit">SOLICITAR INFORMACIÓN</button>
+        <div class="form-bottom">
+            <div class="sf_btn">
+                <button class="btn-content-solid-2" type="submit">SOLICITAR INFORMACIÓN</button>
+            </div>
         </div>
     </form>
 </div>
