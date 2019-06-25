bce18b8e61ba07718cfb76c3be33e8e51dffb687
Juan Kuga <juankp3@gmail.com>
2019-06-25 17:30:32 -0500

    base de datos

--- public/views/home/index.php
+++ public/views/home/index.php
@@ -9,7 +9,7 @@
     <!-- CSS -->
     <link rel="stylesheet" type="text/css" href="assets/css/newstyle.css" >
     <link rel="stylesheet" type="text/css" href="https://superpet.pe/themes/superpet/assets/lib/font-awesome/css/font-awesome.min.css" >
-
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu&display=swap" rel="stylesheet">
 
     <!-- Owl Stylesheets -->
     <link rel="stylesheet" href="assets/owlcarousel/assets/owl.carousel.min.css">
