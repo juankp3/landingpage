a19a34894fd3306c858b8610ae615d665af4a410
Juan Kuga <juankp3@gmail.com>
2019-07-17 20:27:47 -0500

    moviendo galeria

--- views/landing2/index.php
+++ views/landing2/index.php
@@ -18,7 +18,7 @@
     <link rel="stylesheet" href="assets/owlcarousel/assets/owl.theme.default.min.css">
     <script src="assets/vendors/jquery.min.js"></script>
     <script src="assets/owlcarousel/owl.carousel.js"></script>
-    <script src="assets/js/init.js?v=1"></script>
+    <script src="assets/js/init2.js?v=1"></script>
 
     <!-- SEO -->
     <title>Protege la seguridad de tu familia y de tu hogar | Servicios Felix</title>
@@ -60,141 +60,25 @@
         <div class="sf-banner-block02 show-mobile">
             <img src="/assets/img/familia-feliz.jpg" alt="">
         </div>
-        <div class="sf-quehacemos">
-            <div class="content">
-                <div class="block-01  p-block-01">
-                    <h2>¿QUÉ HACEMOS?</h2>
-                    <hr class="separatingline" />
-                    <h4>¡Te ofrecemos la más completa solución!</h4>
-                    <p>Realizamos asesorías, proyectos de remodelación y diseño, servicios de mantenimiento e instalación, muebles a medida y más. Todos con precios a tu alcance.</p>
-                </div>
-            </div>
-        </div>
+        
         <div class="content show-mobile">
             <?php include '_partials/form.php' ?>
         </div>
     </section>
 
-    <section class="sf-container --gray p-block-01 gallery">
-        <div class="content">
-            <div class="block-01">
-                <h2>¡RENUÉVALO TODO!</h2>
-                <p>MIRA NUESTROS PROYECTOS REALIZADOS</p>
-            </div>
-            <?php  include '_partials/gallery.php' ?>
-        </div>
-    </section>
+    
 
     <section class="sf-container servicios">
         <div class="content">
             <div class="block-01">
                 <h2>PREPARA Y PROTEGE TU CASA</h2>
+                <hr class="separatingline" />
                 <p>TENEMOS LA MÁS COMPLETA SOLUCIÓN</p>
             </div>
         </div>
     </section>
 
-    <section class="sf-container --gray pt50 mb8">
-        <div class="content --felx">
-            <div class="sf-content-info">
-                <div class="box-info">
-                    <h2> <span>PREPARACIÓN</span> DE ESTRUCTURAS DE TU CASA PARA CASOS DE <span>SISMOS.</span></h2>
-                    <hr class="separatingline separatingline_left mb24" />
-                    <p>Realízalo en estos 3 pasos:</p>
-                    <ul class="sf-content-list ">
-                        <li><p><strong>Estudio Estructural</strong> - Visita técnica, planos, escaneos, etc.</p></li>
-                        <li><p><strong>Diagnóstico y presupuesto</strong> - Informe y valorización de la solución.</p></li>
-                        <li><p><strong>Ejecución de obra </strong>  - Ejecución de obra - Ponemos en marcha la solución.</p></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="sf-content-img">
-                <img src="/assets/img/w-preparacion-de-estructuras.jpg" class="block-image" alt="Preparación de estrcutura de tu casa para casos de sismos">
-            </div>
-        </div>
-        <img src="/assets/img/preparacion-de-estructuras.jpg" class="ft-image" alt="Preparación de estrcutura de tu casa para casos de sismos">
-    </section>
-    
-    <section class="sf-container --gray pt50 mb8">
-        <div class="content --felx">
-            <div class="sf-content-img">
-                <img src="/assets/img/w-Impermeabilizacion-y-reparacion.jpg" class="block-image" alt="Impermebilización y reparación de cisternas, piscinas y tanques elevados">
-            </div>
-            <div class="sf-content-info">
-                <div class="box-info --right">
-                    <h2><span>IMPERMEABILIZACIÓN</span> Y <span>REPARACIÓN</span> DE CISTERNAS, PICINAS Y TANQUES ELEVADOS</h2>
-                    <hr class="separatingline separatingline_right mb24">
-                    <p>Evita que las lluvias y las fugas deteriore tu cisterna, piscina o tanque elevado. 
-                        Restaurarlas te ayudará a evitar el consumo excesivo de agua. </p>
-                </div>
-            </div>
-        </div>
-            <img src="/assets/img/Impermeabilizacion-y-reparacion.jpg" class="ft-image" alt="Impermebilización y reparación de cisternas, piscinas y tanques elevados">
-    </section>
-
-    <section class="sf-container --gray pt50 mb8">
-        <div class="content --felx">
-            <div class="sf-content-info">
-                <div class="box-info">
-                    <h2 class="break-line"><span>IMPERMEABILIZACIÓN</span> DE TECHOS</h2>
-                    <hr class="separatingline separatingline_left mb24">
-                    <p>Protege tu inmueble de las lluvias. Ten un ambiente más saludable para ti y tu familia. 
-                        Evita las filtraciones, manchas de humedad y moho en muros y techos.</p>
-                </div>
-            </div>
-            <div class="sf-content-img">
-                <img src="/assets/img/w-Impermeabilizacion-de-techos.jpg" class="block-image" alt="Impermebilización de techos">
-            </div>
-        </div>
-        <img src="/assets/img/Impermeabilizacion-de-techos.jpg" class="ft-image" alt="Impermebilización de techos">
-    </section>
-    
-    <section class="sf-container --gray pt50 mb8">
-        <div class="content --felx">
-            <div class="sf-content-img">
-                <img src="/assets/img/w-servicios-especializados.jpg" class="block-image" alt="Servicios Especializados">
-            </div>
-            <div class="sf-content-info ">
-                <div class="box-info --right"">
-                    <h2 class="break-line">SERVICIOS <span>ESPECIALIZADOS</span></h2>
-                    <hr class="separatingline separatingline_right mb24">
-                    <ul class="sf-content-list --right">
-                        <li>Estructuras metálicas</li>
-                        <li>Cableado eléctrico, data y telefonía</li>
-                        <li>Iluminación y rollers</li>
-                        <li>Obra civil, sanitarias</li>
-                        <li>Drywall y falsos cielos</li>
-                        <li>Pintura</li>
-                        <li>Ventanas y mamparas de vídrio</li>
-                        <li>Escaleras metálicas</li>
-                        <li>Servicios Complementarios</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <img src="/assets/img/servicios-especializados.jpg" class="ft-image" alt="Servicios Especializados">
-    </section>
-
-    <section class="sf-container --gray pt50">
-        <div class="content --felx">
-            <div class="sf-content-info">
-                <div class="box-info">
-                    <h2 class="break-line">SERVICIOS <span>COMPLEMENTARIOS</span></h2>
-                    <hr class="separatingline separatingline_left mb24">
-                    <ul class="sf-content-list">
-                        <li>Gasfitería</li>
-                        <li>Electricidad</li>
-                        <li>Mantenimiento e instalaciones</li>
-                        <li>Aire acondicionado</li>
-                    </ul>
-                </div>
-            </div>
-            <div class="sf-content-img">
-                <img src="/assets/img/web-plomero1.jpg" class="block-image" alt="Servicios Complementarios">
-            </div>
-        </div>
-        <img src="/assets/img/plomero1.jpg" class="ft-image" alt="Servicios Complementarios">
-    </section>
+    <?php  include '_partials/services.php' ?>
 
     <section class="sf-container p66 clientes">
         <div class="content">
@@ -214,6 +98,27 @@
                     <?php endforeach ?>
                 </ul>
             </div>
+        </div>
+
+        <div class="sf-quehacemos">
+            <div class="content">
+                <div class="block-01  p-block-01">
+                    <h2>¿QUÉ HACEMOS?</h2>
+                    <hr class="separatingline" />
+                    <h4>¡Te ofrecemos la más completa solución!</h4>
+                    <p>Realizamos asesorías, proyectos de remodelación y diseño, servicios de mantenimiento e instalación, muebles a medida y más. Todos con precios a tu alcance.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="sf-container --gray p-block-01 gallery">
+        <div class="content">
+            <div class="block-01">
+                <h2>¡RENUÉVALO TODO!</h2>
+                <p>MIRA NUESTROS PROYECTOS REALIZADOS</p>
+            </div>
+            <?php  include '_partials/gallery.php' ?>
         </div>
     </section>
 
