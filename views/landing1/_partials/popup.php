449a875db633b213bd9a8d1f2c27c8c654877d19
ladislaox <ladislaox@gmail.com>
2019-07-18 15:31:46 -0500

    modificando titulos de landing1 para seo

--- views/landing1/_partials/popup.php
+++ views/landing1/_partials/popup.php
@@ -1,6 +1,6 @@
 <div id="popup1" class="popup hide_pos_left">
     <div id="closebtn1" class="popup_btn_close">x</div> 
-    <div class="popup_title">¡Sismos! Preparación de estructuras de tu casa para casos de sismos.</div>
+    <div class="popup_title"><h3>¡Sismos! Preparación de estructuras de tu casa para casos de sismos.</h3></div>
     <div class="popup_description">
         <div class="desc_img"><img src="assets/img/icons-terremoto.png" alt=""></div>
         <div class="desc_text">
@@ -18,7 +18,7 @@
 
 <div id="popup2" class="popup hide_pos_right">
     <div id="closebtn2"  class="popup_btn_close">x</div>
-    <div class="popup_title">Filtración de lluvias, fugas en tanques, piscinas o cisternas</div>
+    <div class="popup_title"><h3>Filtración de lluvias, fugas en tanques, piscinas o cisternas</h3></div>
     <div class="popup_description">
         <div class="desc_img"><img class="size_38"  src="assets/img/icons-water.png" alt=""></div>
         <div class="desc_text">Afecta las estructuras e incrementa tu consumo de agua.</div>
@@ -28,9 +28,9 @@
     </div>
 </div>
 
-<div id="popup3" class="popup hide_pos_left"> 
+<div id="popup3" class="popup hide_pos_left">
     <div id="closebtn3"  class="popup_btn_close">x</div> 
-    <div class="popup_title">Impermeabilización de techos</div>
+    <div class="popup_title"><h3>Impermeabilización de techos</h3></div>
     <div class="popup_description">
         <div class="desc_img"><img src="assets/img/icons-rain.png" alt=""></div>
         <div class="desc_text"> Protege tu inmueble de las lluvias.</div>
