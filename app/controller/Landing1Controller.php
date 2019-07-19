2a8735b1cbca6d1825d11665a5b27a65b689b810
ladislaox <ladislaox@gmail.com>
2019-07-18 22:16:52 -0500

    prueba de traking

--- app/controller/Landing1Controller.php
+++ app/controller/Landing1Controller.php
@@ -13,7 +13,7 @@ class Landing1Controller
 
     public function thanks()
     {
-        if(!empty($_POST)) {
+        // if(!empty($_POST)) {
             $post = $_POST;
             $post['tipo'] = 'remodelacion';
             $cone = new Conexion();
@@ -26,9 +26,9 @@ class Landing1Controller
                 Flight::redirect('/');
     
             Flight::render('landing1/thanks.php');
-        } else {
-            Flight::redirect('/remodelacion');
-        }
+        // } else {
+        //     Flight::redirect('/remodelacion');
+        // }
     }
 
 
