e4f58b9204aa0ff77dd1b7e22d4f97835fb6eac3
ladislaox <ladislaox@gmail.com>
2019-07-25 18:06:53 -0500

    agregando clases a botones informate mas aqui de los popups popups landing 1 para tag manager

--- views/landing1/_partials/popup.php
+++ views/landing1/_partials/popup.php
@@ -1,5 +1,5 @@
 <div id="popup1" class="popup hide_pos_left">
-    <div id="closebtn1" class="popup_btn_close  -popup-sismos">x</div> 
+    <div id="closebtn1" class="popup_btn_close  -gtm-popup-close-sismos">x</div> 
     <div class="popup_title"><h3>¡Sismos! Preparación de estructuras de tu casa para casos de sismos.</h3></div>
     <div class="popup_description">
         <div class="desc_img"><img src="assets/img/icons-terremoto.png" alt=""></div>
@@ -12,30 +12,30 @@
         </div>
     </div>
     <div class="popup_button">
-        <button class="btn-content-solid-2 --more-information" data-form="6" type="submit">INFÓRMATE MÁS AQUÍ</button>
+        <button class="btn-content-solid-2 --more-information -gtm-popup-btn-sismos" data-form="6" type="submit">INFÓRMATE MÁS AQUÍ</button>
     </div>
 </div>
 
 <div id="popup2" class="popup hide_pos_right">
-    <div id="closebtn2"  class="popup_btn_close -popup-lluvias">x</div>
+    <div id="closebtn2"  class="popup_btn_close -gtm-popup-close-lluvias">x</div>
     <div class="popup_title"><h3>Filtración de lluvias, fugas en tanques, piscinas o cisternas</h3></div>
     <div class="popup_description">
         <div class="desc_img"><img class="size_38"  src="assets/img/icons-water.png" alt=""></div>
         <div class="desc_text">Afecta las estructuras e incrementa tu consumo de agua.</div>
     </div>
     <div class="popup_button"> 
-        <button class="btn-content-solid-2 --more-information" data-form="7" type="submit">INFÓRMATE MÁS AQUÍ</button>
+        <button class="btn-content-solid-2 --more-information -gtm-popup-btn-lluvias" data-form="7" type="submit">INFÓRMATE MÁS AQUÍ</button>
     </div>
 </div>
 
 <div id="popup3" class="popup hide_pos_left">
-    <div id="closebtn3"  class="popup_btn_close -popup-techos">x</div> 
+    <div id="closebtn3"  class="popup_btn_close -gtm-popup-close-techos">x</div> 
     <div class="popup_title"><h3>Impermeabilización de techos</h3></div>
     <div class="popup_description">
         <div class="desc_img"><img src="assets/img/icons-rain.png" alt=""></div>
         <div class="desc_text"> Protege tu inmueble de las lluvias.</div>
     </div>
     <div class="popup_button">
-        <button class="btn-content-solid-2 --more-information" data-form="8" type="submit">INFÓRMATE MÁS AQUÍ</button>
+        <button class="btn-content-solid-2 --more-information -gtm-popup-btn-techos" data-form="8" type="submit">INFÓRMATE MÁS AQUÍ</button>
     </div>
 </div>
