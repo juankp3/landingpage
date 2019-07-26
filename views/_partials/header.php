7bdd293a5b95e9ebb7bba0c004e17205fed86c3a
ladislaox <ladislaox@gmail.com>
2019-07-25 23:24:02 -0500

    agregando clase boton logo landing 1 para tag manager

--- views/_partials/header.php
+++ views/_partials/header.php
@@ -2,13 +2,13 @@
     <div class="content">
         <div class="logo">
             <!-- <a href="#"><img src="assets/img/isotipo.png" alt=""></a> -->
-            <a href="#"><img src="/assets/img/favicon/servife.png" alt="Servicios Felix"></a>
-            <a href="#"><h1 class="cl-blue">SERVICIOS FELIX</h1></a>
+            <a href="#"><img class="-logo-construccion" src="/assets/img/favicon/servife.png" alt="Servicios Felix"></a>
+            <a href="#"><h1 class="cl-blue -logo-construccion">SERVICIOS FELIX</h1></a>
         </div>
         <div class="cl-whatsapp">
             <a class="sf-btn -whatsapp -web" href="https://api.whatsapp.com/send?phone=51997360983&amp;text=Hola, estoy interesado en información y mi proyecto es">
-                <i class="fab fa-whatsapp mr5 --whatsapp-construccion" aria-hidden="true"></i>
-                <span class="--whatsapp-construccion">997-360-983</span>
+                <i class="fab fa-whatsapp mr5 -whatsapp-construccion" aria-hidden="true"></i>
+                <span class="-whatsapp-construccion">997-360-983</span>
             </a>
         </div>
     </div>
